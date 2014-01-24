2074bab14cebc9e3fb83573055fc6d512ae23b20
walkingriver <mikecallaghan67@gmail.com>
2014-01-24 13:44:38 -0500

    Initial SignalR work done.

---
+++ BuzzerWeb/Buzzer.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BuzzerWeb
+{
+    public class Buzzer : Hub
+    {
+        static string winner = String.Empty;
+        static object locker = new { };
+
+        public void Reset()
+        {
+            lock (locker)
+            {
+                winner = String.Empty;
+            }
+            Clients.All.Reset();
+        }
+
+        public void Buzz()
+        {
+            if (string.IsNullOrEmpty(winner))
+            {
+                lock (locker)
+                {
+                    if (string.IsNullOrEmpty(winner))
+                    {
+                        Clients.Caller.YouWin();
+                        winner = Context.ConnectionId;
+                        Clients.Others.Sorry();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ BuzzerWeb/app/index.js
@@ -0,0 +1,33 @@
+﻿
+$(function () {
+    var $buzz = $('#buzz'),
+        $reset = $('#reset'),
+        hub = $.connection.buzzer;
+
+    hub.client.youWin = function () {
+        alert('YOU WIN');
+    };
+
+    hub.client.sorry = function () {
+        $buzz.attr("disabled", "disabled");
+        alert('Sorry');
+    };
+
+    hub.client.reset = function () {
+        console.log('Reset');
+        $buzz.removeAttr("disabled");
+    };
+
+    $.connection.hub.start().done(function () {
+        // Wire up Reset button to call Reset on the server.
+        $reset.click(function () {
+            hub.server.reset();
+        });
+
+        // Wire up the Buzz button to call Buzz on the server.
+        $buzz.click(function () {
+            $buzz.attr("disabled", "disabled");
+            hub.server.buzz();
+        });
+    });
+});
