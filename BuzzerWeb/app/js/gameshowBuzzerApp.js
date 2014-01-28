f09d86db60ade0f9de0f7223e4f23963538a6d54
walkingriver <mikecallaghan67@gmail.com>
2014-01-28 16:01:43 -0500

    Got the code wokring with Angular bindings instead of jQuery.

---
+++ BuzzerWeb/app/js/gameshowBuzzerApp.js
@@ -0,0 +1,50 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('gameshowBuzzerApp', [])
+        .controller('buzzerController', ['$scope', 'signalRSvc', buzzerController]);
+
+    function buzzerController($scope, signalRSvc) {
+        var vm = this;
+
+        // Bindable properties and functions are placed on vm
+        vm.winner = false;
+        vm.sorry = false;
+        vm.ready = function () { return !winner && !sorry; };
+        vm.title = "Connected";
+        vm.buzz = buzz;
+        vm.reset = reset;
+
+        var _reset = function () {
+            vm.winner = false;
+            vm.sorry = false;
+            vm.title = "You are connected.";
+            $scope.$apply();
+        }
+
+        signalRSvc.initialize();
+
+        $scope.$on('YOU_WIN', function (event, data) {
+            vm.winner = true;
+            $scope.$apply();
+        });
+        $scope.$on('SORRY', function (event, data) {
+            vm.sorry = true;
+            $scope.$apply();
+        });
+        $scope.$on('RESET', function (event, data) {
+            _reset();
+        });
+
+        // These functions are exposed to the outside world through the VM
+        function buzz () {
+            signalRSvc.buzz();
+        }
+
+        function reset() {
+            signalRSvc.reset();
+        };
+
+        _reset();
+    }
+})();
---
+++ BuzzerWeb/app/js/signalRSvc.js
@@ -0,0 +1,51 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('gameshowBuzzerApp')
+        .factory('signalRSvc', ['$rootScope', signalRSvc]);
+
+    function signalRSvc($rootScope) {
+        //Getting the connection object
+        var hub = $.connection.buzzer;
+
+        var initialize = function () {
+            hub.client.youWin = function () {
+                $rootScope.$broadcast('YOU_WIN');
+                console.log('you win');
+            };
+
+            hub.client.sorry = function () {
+                $rootScope.$broadcast('SORRY');
+                $rootScope.$apply();
+                console.log('sorry');
+            };
+
+            var reset = function () {
+                $rootScope.$broadcast('RESET');
+                $rootScope.$apply();
+                console.log('reset');
+            };
+            hub.client.reset = reset;
+
+            //Starting connection
+            $.connection.hub.start().done(function () {
+                // Do something cool here
+            });
+        };
+
+        function sendBuzz() {
+            hub.server.buzz();
+        };
+
+        function sendReset() {
+            hub.server.reset();
+        };
+
+        return {
+            initialize: initialize,
+            buzz: sendBuzz,
+            reset: sendReset
+        };
+    }
+})();
+
